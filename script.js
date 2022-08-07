d9ffed1c0ec3c41b53f26ec6b95f544fcb198972
Kenner Silva <kennerdev@outlook.com>
2022-08-07 18:59:03 -0300

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+let img1 = document.querySelector('#img1')
+
+let depois = document.querySelector('#depois')
+
+img1.addEventListener('click', function() {
+
+     depois.style.display = 'block'
+
+});
